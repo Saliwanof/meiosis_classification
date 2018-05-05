87ac6c9786eb9e244060186689ab28fcc1338c58
Saliwanof <harrypotter_china@126.com>
2018-05-05 13:47:06 +0800

    final_commit

--- custom/models.py
+++ custom/models.py
@@ -6,62 +6,10 @@ from torch.nn import Dropout, Dropout2d, ReLU, Softplus, LeakyReLU
 from torch.nn import BatchNorm2d as Norm2d
 from torch.nn import BatchNorm1d as Norm1d
 
-# activation_func = ReLU(True)
-activation_func = LeakyReLU(0.1, True)
+activation_func = ReLU(True)
+# activation_func = LeakyReLU(0.2, True)
 # activation_func = Softplus()
 
-class Net_Feature_Extraction(nn.Module):
-    '''
-    receptfield of size 20px - 40px needed to detect an anomaly
-    '''
-    def __init__(self):
-        super(Net_Feature_Extraction, self).__init__()
-        
-        self.conv1a = conv_bn(1, 32, 3, 1, 2, 2, True)
-        self.conv1b = conv_bn(32, 32, 3, 1, 2, 2, True)
-        # pool
-        # (32, 112, 112)
-        self.conv2 = inception_v2(32, 64)
-        # pool
-        # (192, 56, 56)
-        self.conv3 = inception_v2(192, 64)
-        # pool
-        # (192, 28, 28)
-        self.conv4 = inception_v2(192, 64)
-        # pool
-        # (192, 14, 14)
-        self.conv5 = Conv2d(192, 16, 1, 1, 0, 1)
-        # (16, 14, 14)
-        # pool
-        # (16, 7, 7)
-        
-        
-    def forward(self, x):
-        x = self.conv1a(x)
-        x = Dropout2d(.2, True)(x)
-        x = self.conv1b(x)
-        x = Dropout2d(.2, True)(x)
-        x = MaxPool2d(2)(x)
-        
-        x = self.conv2(x)
-        x = Dropout2d(.4, True)(x)
-        x = MaxPool2d(2)(x)
-        
-        x = self.conv3(x)
-        x = Dropout2d(.4, True)(x)
-        x = MaxPool2d(2)(x)
-        
-        x = self.conv4(x)
-        x = Dropout2d(.4, True)(x)
-        x = MaxPool2d(2)(x)
-        
-        x = self.conv5(x)
-        x = Dropout2d(.4, True)(x)
-        x = MaxPool2d(2)(x)
-        
-        feature = x.view(-1, 16*7*7)
-        
-        return feature
 
 class Net_Period_Classification(nn.Module):
     '''
@@ -71,55 +19,64 @@ class Net_Period_Classification(nn.Module):
         super(Net_Period_Classification, self).__init__()
         
         self.conv1a = conv_bn(1, 32, 3, 1, 2, 2, True)
+        self.drop1a = Dropout2d(.2, True)
         self.conv1b = conv_bn(32, 32, 3, 1, 2, 2, True)
+        self.drop1b = Dropout2d(.2, True)
         # pool
         # (32, 112, 112)
         self.conv2 = inception_v2(32, 64)
+        self.drop2 = Dropout2d(.4, True)
         # pool
         # (192, 56, 56)
         self.conv3 = inception_v2(192, 64)
+        self.drop3 = Dropout2d(.4, True)
         # pool
         # (192, 28, 28)
         self.conv4 = inception_v2(192, 64)
+        self.drop4 = Dropout2d(.4, True)
         # pool
         # (192, 14, 14)
         self.conv5 = Conv2d(192, 16, 1, 1, 0, 1)
+        self.drop5 = Dropout2d(.4, True)
         # (16, 14, 14)
         # pool
         # (16, 7, 7)
         self.mlp1 = Linear(16*7*7, 64)
+        self.drop01 = Dropout(.2, True)
         self.mlp2 = Linear(64, 64)
+        self.drop02 = Dropout(.2, True)
         self.mlp3 = Linear(64, nclass)
-        
         
     def forward(self, x):
         x = self.conv1a(x)
-        x = Dropout2d(.2, True)(x)
+        x = self.drop1a(x)
         x = self.conv1b(x)
-        x = Dropout2d(.2, True)(x)
+        x = self.drop1b(x)
         x = MaxPool2d(2)(x)
         
         x = self.conv2(x)
-        x = Dropout2d(.4, True)(x)
+        x = self.drop2(x)
         x = MaxPool2d(2)(x)
         
         x = self.conv3(x)
-        x = Dropout2d(.4, True)(x)
+        x = self.drop3(x)
         x = MaxPool2d(2)(x)
         
         x = self.conv4(x)
-        x = Dropout2d(.4, True)(x)
+        x = self.drop4(x)
         x = MaxPool2d(2)(x)
         
         x = self.conv5(x)
-        x = Dropout2d(.4, True)(x)
+        x = self.drop5(x)
         x = MaxPool2d(2)(x)
         
-        x = x.view(-1, 16*7*7)
         feature = x
+        x = x.view(-1, 16*7*7)
         
         x = activation_func(self.mlp1(x))
+        x = self.drop01(x)
         x = activation_func(self.mlp2(x))
+        x = self.drop02(x)
         x = self.mlp3(x)
         target = x
         
